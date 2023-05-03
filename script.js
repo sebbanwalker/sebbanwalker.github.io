15dc6a1b452c0c09005f6747dbada17efcbdfea9
sebbanwalker <seb10walker@hotmail.com>
2023-05-03 15:47:58 +0200

    onload animations

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", function () {
+  /*  this.document.getElementById("photo").style.opacity = 1;
+  this.document.getElementById("intro-text").style.opacity = 1;
+  this.document
+    .getElementById("home-container")
+    .style.transform("translateY: -100%"); */
+  //this.document.getElementById("skills").style.transform("translateY: -35vw");
+  //this.document.getElementById("skills-title__text").style.opacity = 1;
+});
