46852c5df37a7ae7d1236b056a3e9294616986f2
sebbanwalker <seb10walker@hotmail.com>
2023-05-06 00:47:40 +0200

    comingsonntm

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-window.addEventListener("DOMContentLoaded", function () {
-  /*  this.document.getElementById("photo").style.opacity = 1;
-  this.document.getElementById("intro-text").style.opacity = 1;
-  this.document
-    .getElementById("home-container")
-    .style.transform("translateY: -100%"); */
-  //this.document.getElementById("skills").style.transform("translateY: -35vw");
-  //this.document.getElementById("skills-title__text").style.opacity = 1;
+history.scrollRestoration = "manual";
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
+addEventListener("DOMContentLoaded", (event) => {
+  const home = document.getElementById("home-container");
+  home.classList.add("slide-up");
 });
